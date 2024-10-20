826c3435b5d7e825a959724f45ea13a046a732e9
Diego-lg <diegoligarrido@gmail.com>
2024-10-20 10:02:17 -0700

    1.,2

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import ShowCase from './components/ShowcCase';
+
+function App() {
+  return (
+
+    <div className="App">
+
+      <ShowCase />
+    </div>
+  );
+}
+
+export default App;
